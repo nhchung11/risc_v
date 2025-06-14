5ea5b6c95d2248002032ef30a28d20f4ed6c75a2
nhchung07 <ngchung2002bn@gmail.com>
2025-06-15 03:54:21 +0900

    25/06/15: Fix BTB | TODO: ADd flush

--- btb/btb.sv
+++ btb/btb.sv
@@ -9,7 +9,6 @@ module btb
     input logic                 i_rst_n,
     input logic                 i_jump_ID,
     input logic                 i_pc_src_EX,
-    input logic                 i_btb_update_en_ID,
     input logic [WIDTH-1:0]     i_pc_F,
     input logic [WIDTH-1:0]     i_pc_target_ID,
     input logic [WIDTH-1:0]     i_pc_plus4_F,
@@ -19,7 +18,7 @@ module btb
 );
 
     // Wires
-    logic               w_compare;
+    logic               w_hit;
     logic               w_taken;
     logic [WIDTH-1:0]   w_pc_target_F;
     logic [INDEX-1:0]   w_index;
@@ -30,6 +29,8 @@ module btb
     logic [TAG-1:0]     w_btb_tag;
     logic               w_btb_valid;
     logic [WIDTH-1:0]   w_btb_target;
+    logic               w_btb_update_en;
+    logic [INDEX-1:0]   w_hold_index;
     
 
     // BTB entry structure
@@ -43,30 +44,53 @@ module btb
     btb_entry_t btb_array[ENTRY_NUM];
 
     // Combinational logic
-    assign w_pc_sel         = i_jump_ID || i_pc_src_EX;
-    assign o_flush_PC       = ~w_taken && i_jump_ID;
-    assign w_tag            = i_pc_F[WIDTH - 1:INDEX + 2];
-    assign w_index          = i_pc_F[INDEX + 1:2];
-    assign w_btb_target     = btb_array[w_index].target;
-    assign w_btb_predicted  = btb_array[w_index].predicted;
-    assign w_btb_tag        = btb_array[w_index].tag;
-    assign w_btb_valid      = btb_array[w_index].valid;
-    assign w_taken          = w_compare && btb_array[w_index].predicted[1]; 
-    assign w_compare        = (w_btb_tag == w_tag) && w_btb_valid;
+    assign #0.1 w_pc_sel         = i_jump_ID || i_pc_src_EX;
+    assign #0.1 o_flush_PC       = ~w_taken && i_jump_ID;
+    assign      w_tag            = i_pc_F[WIDTH - 1:INDEX + 2];
+    assign      w_index          = i_pc_F[INDEX + 1:2];
+    assign      w_btb_target     = btb_array[w_index].target;
+    assign      w_btb_predicted  = btb_array[w_index].predicted;
+    assign      w_btb_tag        = btb_array[w_index].tag;
+    assign      w_btb_valid      = btb_array[w_index].valid;
+    assign #0.1 w_taken          = w_hit && btb_array[w_index].predicted[1]; 
+    assign #0.1 w_hit            = (w_btb_tag == w_tag) && w_btb_valid;
+    assign #0.1 w_btb_update_en  = w_pc_sel && ~w_hit;
 
 
+    // Update BTB
     always_ff @(posedge i_clk or negedge i_rst_n) begin
         if (!i_rst_n) begin
             for (int i = 0; i < ENTRY_NUM; i++) begin
                 btb_array[i].valid      <= 1'b0;
             end
-        end else if (i_btb_update_en_ID) begin
-            btb_array[w_index].tag      <= i_pc_F[w_tag];
-            btb_array[w_index].target   <= i_pc_target_ID;
-            btb_array[w_index].valid    <= 1'b1;
+        end else if (w_btb_update_en) begin
+            btb_array[w_hold_index].tag      <= i_pc_F[w_tag];
+            btb_array[w_hold_index].target   <= i_pc_target_ID;
+            btb_array[w_hold_index].valid    <= 1'b1;
         end
     end
 
+    // Update BTB prediction
+    always_ff @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            for (int i = 0; i < ENTRY_NUM; i++) begin
+                btb_array[i].predicted <= 2'b00; // Initialize all predictions to not taken
+            end
+        end else if (w_btb_update_en) begin
+            btb_array[w_hold_index].predicted <= 2'b10;
+        end
+    end
+
+    // Hold index
+    always_ff @(posedge i_clk or negedge i_rst_n) begin
+        if (!i_rst_n) begin
+            w_hold_index <= 0;
+        end else begin
+            w_hold_index <= w_index;
+        end
+    end
+
+    // MUX0 - select target PC based on taken or not
     always_comb begin
         if (w_taken) begin
             w_pc_target_F = w_btb_target;
@@ -75,8 +99,9 @@ module btb
         end
     end
 
+    // MUX1 - select PC for next cycle
     always_comb begin
-        if (w_pc_sel) begin
+        if (w_pc_sel | w_taken) begin
             o_pc_F0 = w_pc_target_F;
         end else begin
             o_pc_F0 = i_pc_plus4_F;
--- btb/tb.sv
+++ btb/tb.sv
@@ -9,30 +9,29 @@ module tb;
     logic i_rst_n;
     logic i_jump_ID;
     logic i_pc_src_EX;
-    logic i_btb_update_en_ID;
     logic [WIDTH-1:0] i_pc_target_ID;
     logic [WIDTH-1:0] i_pc_plus4_F;
     logic o_flush_PC;
     logic [WIDTH-1:0] o_pc_F0;
     logic [31:0] reg_pc;
     logic [WIDTH-1:0] i_pc_F;
+    logic [WIDTH-1:0] pc_plus4;
 
     assign i_pc_F = reg_pc; // Connect reg_pc to i_pc_F
     // Instantiate the DUT
     btb #(
-        .WIDTH(WIDTH),
-        .ENTRY_NUM(ENTRY_NUM)
+        .WIDTH          (WIDTH),
+        .ENTRY_NUM      (ENTRY_NUM)
     ) dut (
-        .i_clk(i_clk),
-        .i_rst_n(i_rst_n),
-        .i_jump_ID(i_jump_ID),
-        .i_pc_src_EX(i_pc_src_EX),
-        .i_btb_update_en_ID(i_btb_update_en_ID),
-        .i_pc_F(reg_pc),
-        .i_pc_target_ID(i_pc_target_ID),
-        .i_pc_plus4_F(i_pc_plus4_F),
-        .o_flush_PC(o_flush_PC),
-        .o_pc_F0(o_pc_F0)
+        .i_clk          (i_clk),
+        .i_rst_n        (i_rst_n),
+        .i_jump_ID      (i_jump_ID),
+        .i_pc_src_EX    (i_pc_src_EX),
+        .i_pc_F         (reg_pc),
+        .i_pc_target_ID (i_pc_target_ID),
+        .i_pc_plus4_F   (pc_plus4),
+        .o_flush_PC     (o_flush_PC),
+        .o_pc_F0        (o_pc_F0)
     );
     always_ff @(posedge i_clk or negedge i_rst_n) begin
         if (~i_rst_n) begin
@@ -40,6 +39,28 @@ module tb;
         end else begin
             reg_pc <= o_pc_F0; // Update PC with the output from the DUT
         end
+    end
+
+   
+    assign pc_plus4 = reg_pc + 4; // Calculate PC + 4
+   
+
+    // Write data task
+    task write(input logic [31:0] pc_target, logic jump);
+        @(posedge i_clk);
+        #1; 
+        i_jump_ID = jump;
+        i_pc_target_ID = pc_target; 
+        if (jump) begin
+            $display("Update at index: %0d", dut.w_hold_index);
+            $display("Updating BTB with target: %h", i_pc_target_ID);
+        end
+    endtask
+
+    initial begin
+        if ($time == 76) force i_jump_ID = 1;
+        if ($time == 86) force i_jump_ID = 0;
+        release i_jump_ID;
     end
 
     // Clock generation
@@ -54,9 +75,7 @@ module tb;
         i_rst_n = 0;
         i_jump_ID = 0;
         i_pc_src_EX = 0;
-        i_btb_update_en_ID = 0;
         i_pc_target_ID = 0;
-        i_pc_plus4_F = 0;
     
         for (int i = 0; i < 32; i++) begin
             dut.btb_array[i].tag = 0; // Set all entries to zero
@@ -68,29 +87,18 @@ module tb;
         #15;
         i_rst_n = 1;
 
-        // Wait for reset to complete
+        // Test case 1: Write to BTB
+        write(32'h00000000, 0); 
+        write(32'h00000000, 0); 
+        write(32'h00000020, 1); 
+        write(32'h00000000, 0); 
+        write(32'h00000008, 1); 
+        write(32'h00000000, 0); 
+        write(32'h00000000, 0); 
+        #100;
         @(posedge i_clk);
-        #1;
-        i_pc_plus4_F = 32'h00000004; 
-        @(posedge i_clk);
-        #1;
-        i_pc_plus4_F = 32'h00000008; 
-        @(posedge i_clk);
-        #1;
-        i_pc_plus4_F = 32'h0000000C;
-        i_btb_update_en_ID = 1; 
-        i_jump_ID = 1; 
-        #0.5;
-        i_pc_target_ID = 32'h00000028;
-        $display("Update at index: %0d", dut.w_index);
-        $display("Updating BTB with target: %h", i_pc_target_ID);
-
-        @(posedge i_clk);
-        #1;
-        i_jump_ID = 1; 
-        i_btb_update_en_ID = 0;
-        
-        #200;
+        i_rst_n = 0; // Reset the DUT
+        #20;
         // Finish simulation
         $finish;
     end
